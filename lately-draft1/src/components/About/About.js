13d29c43ae9536827cde4506f9462d4fc5fd1a6b
jacobcarey93 <31251057+jacobcarey93@users.noreply.github.com>
2017-12-08 11:18:32 -0700

    updated about page.

--- lately-draft1/src/components/About/About.js
+++ lately-draft1/src/components/About/About.js
@@ -16,12 +16,7 @@ class About extends Component {
           <div className='about_content'>LATELY.FM MUSIC YOU SHOULD BE LISTENING TO</div>
           <br />
           <br />
-          <div className='about_content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          <div className='about_content'>This is a proof of concept full stack web app for Lately.Fm. This is a test environment only.</div>
         </div>
       </div>
     );
